0459e6d403709fe046a71e4d53f8fc714eff093d
IamStemCell <113768157+IamStemCell@users.noreply.github.com>
2023-03-08 14:39:43 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,9 +2,9 @@
  print ("hello, World") 
  # 3__5__8__13__21__35__55 
  print ( 3,5,8,13,21,35,55, sep ='__') 
- # 'Persha Line'  
- print ('Persha Line', end=' ') 
- # 'Second Line' with no filling empty line between previous line 
+ # 'This is a first line'  
+ print ('This is a first line', end=' ') 
+ # 'Second Line' with no empty line between previous line 
  print ("Second Line", end = '\n\n\n\n\n') 
  # 'Final line' starting from 5 empty lines after previous one  
  print ("Final line")
