4c6091a1acb55079ff9dba90875ba2d352dab68d
IamStemCell <113768157+IamStemCell@users.noreply.github.com>
2023-03-12 02:29:39 +0200

    Create Lesson3(11.03)HmWrk.py

---
+++ Lesson3(11.03)HmWrk.py
@@ -0,0 +1,37 @@
+#Task1 
+#The greeting program.
+
+name = "Svitlana"
+day = "Saturday"
+#1 way using arguments of print function
+print ("Good day",name,"!",day,"is a perfect day to learn some python.")
+#2 way using formatting string 
+print("Good day {}! {} is a perfect day to learn some python.".format(name,day))
+#3 way using concatenation
+print("Good day " + name + "! " + day + " is a perfect day to learn some python.")
+
+#4 f-formatting string
+print(f"Good day {name}! {day} is a perfect day to learn some python.")
+
+
+#Task 2
+#Manipulate strings.
+
+first_name = "svitlana"
+last_name = "poly"
+
+print("Hello, " + first_name.title() + " " + last_name.title())
+
+
+#Task 3 
+#Using python as a calculator.
+
+a = 8
+b = 6
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print (a**b)
+print (a%b)
+print (a//b)
